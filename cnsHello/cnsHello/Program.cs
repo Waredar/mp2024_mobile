a6f6435d529dde52240db14df208a2157ed81311
Waredar <danilka20155@mail.ru>
2024-09-07 13:20:28 +0300

    labs1

---
+++ cnsHello/cnsHello/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите имя:");
+        string? name = Console.ReadLine();
+
+        Console.WriteLine("Введите город:");
+        string? city = Console.ReadLine();
+
+
+        Console.WriteLine("Имя = " + name + ", Город = " + city);
+        Console.WriteLine("Имя =  {0}, Город = {1}", name, city);
+        Console.WriteLine($"Имя =  {name}, Город = {city}");
+        
+         
+    }
+}
